59981390c9d5016939ea61b168d27559fde3212a
Nandom Gusen <nandom@swipe.ng>
2020-06-23 09:46:46 +0100

    Update on CrossOrigin

--- src/main/java/com/itech/Flint/Controller/FlintController.java
+++ src/main/java/com/itech/Flint/Controller/FlintController.java
@@ -11,10 +11,7 @@ import com.itech.Flint.Service.ApiResponse;
 import com.itech.Flint.Service.FlintService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,6 +20,8 @@ import java.util.List;
  * @author Nandom Gusen
  */
 @RestController
+
+@CrossOrigin(origins = "http://localhost:4200")
 public class FlintController {
 
     @Autowired
